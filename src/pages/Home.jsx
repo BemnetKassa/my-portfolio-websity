696aed80eecac2efe6640bd4b755c5d5cde21cf0
BemnetKassa <bemnetkassa79@gmail.com>
2025-06-15 22:12:20 +0300

    starting my home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+<section className="heroSection">
+      <nav className="navbar">
+    <div className="logo">Bemnet Kassa</div>
+    <ul className="nav-links">
+      <li><a href="#about">About</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+  </nav>
+
+
+  <div className="hero-content">
+    <div className="text-area">
+      <p>Hello.</p>
+      <h1>I’m Bemnet Kassa</h1>
+      <h2>Web Developer</h2>
+      <div className="buttons">
+        <a href="#contact" className="btn primary">Got a project?</a>
+        <a href="/your-resume.pdf" className="btn secondary" target="_blank">My resume</a>
+      </div>
+    </div>
+    <div className="image-area">
+      <img src="/your-photo.png" alt="Profile" />
+    </div>
+  </div>
+
+  <div className="tech-strip">
+    <span>HTML5</span><span>CSS</span><span>JavaScript</span><span>React</span><span>Node.js</span><span>Git</span>
+  </div>
+  </section>
